81038e87b717ad8468dd91bb6580645087a5cd1c
Abram1r <103975640+Abram1r@users.noreply.github.com>
2022-06-06 23:39:47 -0500

    added catch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,10 +9,16 @@ var getUserRepos = function(user) {
 
   // make a get request to url
   fetch(apiUrl).then(function(response) {
-    console.log(response);
+    if (response.ok) {
     response.json().then(function(data) {
       displayrepos(data, user);
     });
+ } else {
+     alert("Error: Github User Not Found");
+ }
+  })
+  .catch(function(error) {
+      alert("Unable to connect to Github");
   });
 };
 
@@ -29,6 +35,11 @@ var formSubmitHandler = function(event) {
 };
 
 var displayrepos = function(repos, searchTerm) {
+    // check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     console.log(repos);
     console.log(searchTerm);
     repoContainerEl.textContent = "";
